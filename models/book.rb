775d3933e7bbf8fb446d897eec98844180539633
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-08 00:04:30 -0500

    book model

---
+++ models/book.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# Saved book
+class Book < LazyRecord
+  include Enumerable
+  attr_reader :cover, :title, :authors, :date, :uid
+  attr_accessor :notes, :state
+
+  def initialize(book_obj, state)
+    @state = state
+    @date = Time.now
+    @uid = book_obj.uid
+    @title = book_obj.title
+    @cover = book_obj.thumbnail
+    @authors = book_obj.authors
+    @notes = ''
+  end
+
+  def update_notes!(notes)
+    @notes = notes
+    save
+  end
+
+  def update_state!(state)
+    @state = state
+    save
+  end
+end
